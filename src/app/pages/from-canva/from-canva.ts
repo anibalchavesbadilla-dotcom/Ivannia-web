63df30f871002a3336234f8d4252eeec6036e0d7
Anibal Chaves <anibal.chaves.badilla@gmail.com>
2025-09-25 23:43:25 -0600

    update

--- src/app/pages/from-canva/from-canva.ts
+++ src/app/pages/from-canva/from-canva.ts
@@ -1,16 +1,17 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-from-canva',
-  imports: [NgFor],
+  imports: [NgFor,NgIf],
   templateUrl: './from-canva.html',
   styleUrl: './from-canva.css'
 })
 export class FromCanva implements OnInit {
   canva: SafeResourceUrl | undefined;
+  images: any[] = [];
 
   constructor(private httpClient:HttpClient,private sanitizer: DomSanitizer) {}
 
@@ -18,6 +19,10 @@ export class FromCanva implements OnInit {
     this.httpClient.get("https://ivannia-miller-vasquez-default-rtdb.firebaseio.com/canva.json").subscribe((datos:any)=>{
       let url = datos.url
       this.canva = this.sanitizer.bypassSecurityTrustResourceUrl(url+"/view?embed")
+    })
+
+    this.httpClient.get("https://ivannia-miller-vasquez-default-rtdb.firebaseio.com/images.json").subscribe((datos:any)=>{
+      this.images = datos.filter((f:any) => f);
     })
   }
 
